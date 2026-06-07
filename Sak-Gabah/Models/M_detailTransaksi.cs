8f9073bfd196c0eeeeb697694b5e449cb81f68bb
ABEEENG <nabilamin0123@gmail.com>
2026-06-07 13:37:59 +0700

    Renaming

---
+++ Sak-Gabah/Models/M_detailTransaksi.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sak.Gabah
+{
+    public class DetailTransaksi
+    {
+        public int id_detail { get; set; }
+        public int id_transaksi { get; set; }
+        public int id_detail_komoditas { get; set; } 
+        public int kuantitas { get; set; }
+    }
+}
